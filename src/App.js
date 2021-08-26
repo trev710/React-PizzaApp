20ddb38c70a1da3d7acb29e7fca3fdc2020b0712
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-08-26 14:03:06 -0600

    minor changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import $ from 'jquery';
 import axios from 'axios';
 
-//The browser router - we wrap the app in it, so that we allow routing
-//Also, route
+
+//route
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
@@ -20,8 +20,7 @@ import IngredientList from './Ingredients/IngredientList.js';
 import Layout from './Layout/Layout.js';
 import CheckoutPage from './Ordering/CheckoutPage.js';
 
-//Defaults for axios
-axios.defaults.baseURL = 'https://burgerapp-a9f69.firebaseio.com';
+
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import * as serviceWorker from './serviceWorker';
 import pizzaR from './store/reducers/pizzaComposition.js';
 import pizzaB from './store/reducers/pizzaBuild.js';
 
-//Store and combine reducers (since using more than 1)
-//Also provider so we can give the store to it
+
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 
@@ -22,8 +21,3 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
