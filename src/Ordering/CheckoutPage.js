f28c8e2e9b255395e221d2ac0b02e0eefd5a0762
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-08-27 10:32:20 -0600

    component fixes

--- src/Ordering/CheckoutPage.js
+++ src/Ordering/CheckoutPage.js
@@ -47,24 +47,17 @@ class CheckoutPage extends Component
     };
   }
 
-  //Generally, this can be used for all input types
   generalInputModifier = (event, identifier) =>
   {
-    //Original value of order form - get it now
-    //and clone it (not just reference copying)
-    //Don't directly modify it -> recall that we should use setState instead
+ 
     const originalOrderForm = {...this.state.orderForm};
 
-    //Create a copy of that part of the object that we need.
     const newModification = {...originalOrderForm[identifier]};
 
-    //Assign the value - why we're here
     newModification.value = event.target.value;
 
-    //And now to set the only thing that we changed.
     originalOrderForm[identifier] = newModification;
 
-    //Finally, set the state
     this.setState({orderForm: originalOrderForm},
         ()=> {
           this.verifyInput(identifier);
@@ -73,55 +66,22 @@ class CheckoutPage extends Component
 
   }
 
-  //Checked box for coupon
-  //So we toggle
-  checkedCoupon = () => {
-
-    //The whole variable since we're using a nested object!
-    const modifiedState = {...this.state.orderForm};
-
-    //The one we need
-    const newValuesCheckbox = {...modifiedState['hasCoupon']};
-
-    //and the coupon code
-    const newValuesCoupon = {...modifiedState['couponCode']};
-
-    //Not, modification time
-    newValuesCoupon.elementConfig.disabled = newValuesCheckbox.elementConfig.checked;
-    newValuesCheckbox.elementConfig.checked = !newValuesCheckbox.elementConfig.checked;
-    newValuesCoupon.value = '';
-
-    modifiedState['hasCoupon'] = newValuesCheckbox;
-    modifiedState['couponCode'] = newValuesCoupon;
-
-    //Set state, after re-verification
-    this.setState({orderForm: modifiedState}, () => {this.verifyInput('couponCode')});
-  }
-
-
 
   resetButtonHandler = () => {
  
   }
 
-  //Click on submit -> we must verify the input
   submitButtonHandler = () => {
-    //Step by step -> if error foudn stop, so that it's not overwritten
-    if(this.state.isValid && this.verifyInput('name') && this.verifyInput('email') && this.verifyInput('couponCode'))
-    {
-      alert('Submitted!');
-    }
+        if(this.state.isValid && this.verifyInput('name') && this.verifyInput('email') && this.verifyInput('couponCode'))
+        {
+          alert('Order Submitted');
+        }
+    
 
   }
-
-  //Verify the input - each case has different options
   verifyInput = (elementKey) => {
-    //We'll get a validity and message(s)
     let validity = true;
     let messages = [];
-
-
-    // let checkResult;
 
 
     let copyState = {...this.state};
@@ -139,15 +99,12 @@ class CheckoutPage extends Component
   }
   standardVerification = (value, rule) =>
   {
-    //Start from true, can only go to negative
     let isValid = {outcome: true, message: ''};
 
-    //Switch case for possibilities
     switch(rule.type)
     {
   }
 
-    //Remove message if still ok
     if(isValid.outcome)
     {
       isValid.message = "";
@@ -156,8 +113,6 @@ class CheckoutPage extends Component
     return isValid;
   }
 
-
-  //The render method
   render(){
     let ingredientsInfoArray = [];
 
@@ -192,9 +147,6 @@ class CheckoutPage extends Component
       </div>
     );
 
-
-
-
     const formArray = [];
 
     Object.keys(this.state.orderForm).forEach(key => {
@@ -210,8 +162,6 @@ class CheckoutPage extends Component
       errorClassForm = "checkoutForm hasError";
     }
 
-
-    //Note: prepare the event handler -> it takesthe event and the id of the element
     let form = (
       <form className={errorClassForm}>
         {
@@ -240,16 +190,7 @@ class CheckoutPage extends Component
         {ingredientInfo}
         <h2>Checkout info:</h2>
         {form}
-        {/* <div className="errorMsgs">
-          {
-            this.state.errorMessages.map((errorMsg, index) => {
-              if(errorMsg !== '')
-              {
-                return <h2 key={'error'+index}>{errorMsg}</h2>;
-              }
-            })
-          }
-        </div> */}
+  
       </div>
 
       );
